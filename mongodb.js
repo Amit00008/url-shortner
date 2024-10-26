20386deb48182218a73e4f9e92f42b8864bba569
Amit dey <101701030+Amit00008@users.noreply.github.com>
2024-10-26 20:26:15 +0530

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const mongoConnect = async ( ) => {
-    await mongoose.connect("mongodb://0.0.0.0:27017/test-db").then(()=>{
+    await mongoose.connect("mongodb+srv://amit:870574046@urlshort.f7vdk.mongodb.net/?retryWrites=true&w=majority&appName=urlshort").then(()=>{
         console.log('Connected to database')
     })
     .catch((err)=>{
